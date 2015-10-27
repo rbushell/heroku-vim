7c944a242864c48661c36f28e212bc93688b69a6
Rick Bushell <rickbushell@gmail.com>
2015-10-27 09:04:26 -0700

    Update init.rb
    'cos vim is too hard to type

--- init.rb
+++ init.rb
@@ -18,6 +18,7 @@ class Heroku::Command::Vim < Heroku::Command::Run
     <<-CMD
 mkdir vim
 curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
+ln -s /app/vim/bin/vim /app/vim/bin/vi
 export PATH=$PATH:/app/vim/bin
 bash
     CMD
